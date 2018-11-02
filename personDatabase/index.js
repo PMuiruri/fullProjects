ac5ac6c1ddb5a8c331e51dde15d92a037975c373
PMuiruri <peninah.m.muiruri@gmail.com>
2018-11-02 18:31:47 +0200

    firs full project created basic html and database connection file

---
+++ personDatabase/index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const http=require('http');
+const path=require('path');
+const express=require('express');
+
+const app=express();
+const {host,port,debug}=require('./serverConfig');
+
+const server=http.createServer(app);
+//this will require the initDataStorage function and call it with debug parameter value from serverConfig.json
+const personStorage=require('./personDb')(debug);
+
+const statusHandling=[sendErrorPage,sendStatusPage];
+
+const getroutes= require('./routes/getroutes')(personStorage,...statusHandling);
+const insertroutes=require('./routes/insertroutes')(personStorage,...statusHandling);
+
+app.use(express.urlencoded({extended:false}));
+
+app.set('view engine','ejs');
+app.set('views', path.join(__dirname,'pageViews'));
+
+app.use(express.static(path.join(__dirname,'public')));
+app.use('/',getroutes);
+app.use('/',insertroutes);
+app.get('/',(req,res)=>res.sendFile(path.join(__dirname,'menu.html')));
+
+server.listen(port,host,()=>
+/*eslint-disable no-console*/
+	console.log(`Server ${host} running at ${port}`));
+
+function sendErrorPage(res, message='Error', title='Error', header='Error') {
+	sendStatusPage(res, message, title,header);
+}
+
+function sendStatusPage(res, message='Status', title='Status', header='Status'){
+	return res.render('statusPage',{title:title,header:header,message:message});
+}
